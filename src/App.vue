37603e624c4a8b134ed939814dc02130dffbb56b
Champion-Ming <1914816910@qq.com>
2021-02-22 23:34:08 +0800

    feat:全局导入了Vant组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <h1>123</h1>
+    <i class="toutiao-icon toutiao-iconpinglun"></i>
 <router-view></router-view>
+<van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
